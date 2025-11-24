6dc6575e777ac2791baace8df646caad60c1633e
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-11-24 00:00:13 -0400

    Creacion de entidad instituciones

--- Model/Institucion.cs
+++ Model/Institucion.cs
@@ -1,6 +1,18 @@
-﻿namespace PublicadoraMagna.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PublicadoraMagna.Model;
+
+public class Institucion
 {
-    public class Institucion
-    {
-    }
+    [Key]
+    public int InstitucionId { get; set; }
+    [Required]
+    public string Nombre { get; set; }
+    public string Rnc { get; set; }
+    public string CorreoContacto { get; set; }
+    public string Telefono { get; set; }
+    public DateTime FechaRegistro { get; set; }
+
+
+    public ICollection<Usuario> Usuarios { get; set; }
 }
